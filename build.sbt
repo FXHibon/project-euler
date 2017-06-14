4d85f4ea306c295be008c94982237a54cdf7bda9
FXHibon <hibonfx@gmail.com>
2017-06-14 19:39:03 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,14 @@
+name := "project-euler"
+
+version := "1.0-SNAPSHOT"
+
+scalaVersion := "2.12.2"
+
+libraryDependencies ++= Seq(
+  "ch.qos.logback" % "logback-classic" % "1.1.7",
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.5.0",
+  "com.typesafe.akka" %% "akka-stream" % "2.5.2",
+  "com.typesafe.akka" %% "akka-stream-testkit" % "2.5.2" % "test",
+  "org.scalactic" %% "scalactic" % "3.0.1",
+  "org.scalatest" %% "scalatest" % "3.0.1" % "test"
+)
---
+++ src/main/scala/Problem1.scala
@@ -0,0 +1,20 @@
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
+import com.typesafe.scalalogging.LazyLogging
+
+/**
+  * Created by fx on 14/06/2017.
+  * @see https://projecteuler.net/problem=1
+  */
+object Problem1 extends App with LazyLogging  {
+
+  implicit val system = ActorSystem()
+  implicit val materializer = ActorMaterializer()
+  implicit val executionContext = materializer.executionContext
+
+  Source(0 to 999)
+    .filter { e => (e % 3 == 0) || (e % 5 == 0) }
+    .runFold(0)(_ + _)
+    .map { res => logger.info(s"Sum of all the multiples of 3 or 5 below 1000 is $res") }
+}
