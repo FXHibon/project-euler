95046ad6014b755249ee0ea8739b238a8efd7293
François-Xavier Hibon <francois-xavier.hibon@iadvize.com>
2017-06-18 05:19:15 +0200

    feat(problem4)

--- src/main/scala/com/fxhibon/euler/problem4/Problem4.scala
+++ src/main/scala/com/fxhibon/euler/problem4/Problem4.scala
@@ -2,6 +2,8 @@ package com.fxhibon.euler.problem4
 
 import com.typesafe.scalalogging.LazyLogging
 
+import scala.collection.mutable
+
 /**
   * Created by fx on 17/06/2017.
   *
@@ -11,23 +13,28 @@ object Problem4 extends App with LazyLogging {
 
   val (min, max) = (100, 999)
 
-  def isPalyndrom(i: Int): Boolean = {
-    i.toString match {
-      case even if even.length % 2 == 0 =>
-        even.substring(0, even.length / 2) == even.substring(even.length / 2).reverse
-      case odd =>
-        odd.substring(0, (odd.length - 1) / 2) == odd.substring((odd.length + 1) / 2).reverse
-    }
+  def isPalyndrom(i: Int): Boolean = i.toString == i.toString.reverse
+
+  case class Palyndrom(op1: Int, op2: Int) {
+    val res: Int = op1 * op2
   }
 
+  object Palyndrom {
+    implicit val ordering: Ordering[Palyndrom] = (x: Palyndrom, y: Palyndrom) => x.res - y.res
+  }
 
-  for (x <- (min to max).reverse) {
-    for (y <- (min to max).reverse) {
+  val pals = mutable.ListBuffer.empty[Palyndrom]
+  for (x <- min to max) {
+    for (y <- min to max) {
       if (isPalyndrom(x * y)) {
-        logger.info(s"Largest palyndrom is ${x * y} = $x x $y")
-        sys.exit()
+        pals += Palyndrom(x, y)
       }
     }
   }
+
+  val res = pals.max
+
+  logger.info(s"Largest palyndrom is ${res.op1} x ${res.op2} = ${res.res}")
+
 
 }
