43961a6573eae6073a06916fe0cb04c5a66a638f
FXHibon <hibonfx@gmail.com>
2017-07-02 19:14:35 +0200

    feat(problem6)

---
+++ src/main/scala/com/fxhibon/euler/Problem6.scala
@@ -0,0 +1,36 @@
+package com.fxhibon.euler
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
+import com.typesafe.scalalogging.LazyLogging
+
+/**
+  * Created by fx on 19/06/2017.
+  *
+  * @see https://projecteuler.net/problem=6
+  */
+object Problem6 extends App with LazyLogging {
+
+  implicit val system = ActorSystem()
+  implicit val materializer = ActorMaterializer()
+  implicit val executionContext = materializer.executionContext
+
+  val source = Source(1 to 100)
+
+  val futureRes = for {
+    sumOfSquares <- source.runFold(0)((prev, cur) => prev + (cur * cur))
+    squaresOfSum <- source.runFold(0)(_ + _).map(sum => sum * sum)
+  } yield {
+    logger.info(s"sumOfSquares = $sumOfSquares")
+    logger.info(s"squaresOfSum = $squaresOfSum")
+    squaresOfSum - sumOfSquares
+  }
+
+  futureRes.map { res =>
+    logger.info(s"The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is $res")
+  }.onComplete { _ =>
+    system.terminate()
+  }
+
+}
