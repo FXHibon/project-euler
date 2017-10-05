bc53c7f2fa9f35904a3fde0d1a3c8f0cf6dfa39d
FXHibon <hibonfx@gmail.com>
2017-10-05 20:45:34 +0200

    feat(plugins): archi + tests

--- src/main/scala/com/fxhibon/euler/problem1/Problem1.scala
+++ src/main/scala/com/fxhibon/euler/problem1/Problem1.scala
@@ -1,27 +1,16 @@
 package com.fxhibon.euler.problem1
 
-import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{ Keep, Source }
-import com.typesafe.scalalogging.LazyLogging
+import domain.Problem
 
 /**
  * Created by fx on 14/06/2017.
  *
  * @see https://projecteuler.net/problem=1
  */
-object Problem1 extends App with LazyLogging {
-
-  implicit val system = ActorSystem()
-  implicit val materializer = ActorMaterializer()
-  implicit val executionContext = materializer.executionContext
-
-  Source(0 to 999)
+class Problem1 extends Problem[Int] {
+  override val stream = Source(0 to 999)
     .filter { e => (e % 3 == 0) || (e % 5 == 0) }
+    .reduce(_ + _)
     .watchTermination()(Keep.right)
-    .runFold(0)(_ + _)
-    .map { res => logger.info(s"Sum of all the multiples of 3 or 5 below 1000 is $res") }
-    .onComplete { _ =>
-      system.terminate()
-    }
 }
